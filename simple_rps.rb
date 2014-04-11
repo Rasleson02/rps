fd91ef17c21cd0e257c130501fea35766be92a6a
Rasleson02 <Rasleson02>
2014-04-11 17:51:15 +0000

    Made a sample rock paper scissors game in 8 lines useing a hash

--- simple_rps.rb
+++ simple_rps.rb
@@ -1,6 +1,8 @@
-OUTCOMES={"rock"=>{"rock" => "tie","paper",=>"player 2","sissors"=>"player 1"},
-         "paper"=>{},
-         "scissors"=>{} 
-  }
-
-
+OUTCOMES={"rock"=>{"rock" => "tie","paper"=>"player 2","scissors"=>"player 1"},
+         "paper"=>{"paper"=> "tie","rock"=>"player 1","scissors"=>"player 2"},
+"scissors"=>{"scissors"=> "tie","paper"=>"player 1","rock"=>"player 2"}} 
+  
+computer_choice=["rock","paper","scissors"].sample
+puts"pick rock paper or scissors and try to bet the computer"
+user_choice=gets.chomp
+puts OUTCOMES[user_choice][computer_choice]
